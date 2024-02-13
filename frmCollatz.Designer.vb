31132f5e866c380f5f4e7a2c16e35fb773afeed6
Scott Wegley <scbrwegley@protonmail.ch>
2024-02-13 15:53:19 -0800

    part 1 of collatz conjecture finished, adventurous part not currently complete

--- frmCollatz.Designer.vb
+++ frmCollatz.Designer.vb
@@ -22,17 +22,52 @@ Partial Class frmCollatz
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnLoopCollatz = New System.Windows.Forms.Button()
+        Me.lbxSingleCollatz = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(13, 13)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(113, 20)
+        Me.txtInput.TabIndex = 0
+        '
+        'btnLoopCollatz
+        '
+        Me.btnLoopCollatz.Location = New System.Drawing.Point(13, 40)
+        Me.btnLoopCollatz.Name = "btnLoopCollatz"
+        Me.btnLoopCollatz.Size = New System.Drawing.Size(113, 23)
+        Me.btnLoopCollatz.TabIndex = 1
+        Me.btnLoopCollatz.Text = "Iterate From Value"
+        Me.btnLoopCollatz.UseVisualStyleBackColor = True
+        '
+        'lbxSingleCollatz
+        '
+        Me.lbxSingleCollatz.ColumnWidth = 20
+        Me.lbxSingleCollatz.FormattingEnabled = True
+        Me.lbxSingleCollatz.Location = New System.Drawing.Point(133, 13)
+        Me.lbxSingleCollatz.Name = "lbxSingleCollatz"
+        Me.lbxSingleCollatz.Size = New System.Drawing.Size(120, 433)
+        Me.lbxSingleCollatz.TabIndex = 2
         '
         'frmCollatz
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbxSingleCollatz)
+        Me.Controls.Add(Me.btnLoopCollatz)
+        Me.Controls.Add(Me.txtInput)
         Me.Name = "frmCollatz"
         Me.Text = "Collatz Conjecture"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnLoopCollatz As Button
+    Friend WithEvents lbxSingleCollatz As ListBox
 End Class
--- frmCollatz.vb
+++ frmCollatz.vb
@@ -1,3 +1,48 @@
 ﻿Public Class frmCollatz
 
+    Const intLoopLimit As Integer = 10000
+
+    Private Function iterateCollatz(ByVal intN As Integer) As Integer
+        If intN = 0 Then Return 0
+
+        If intN Mod 2 = 0 Then
+            Return intN / 2
+        Else
+            Return 3 * intN + 1
+        End If
+    End Function
+
+    Private Function loopCollatz(ByVal intN As Integer, ByRef lbxOutput As ListBox) As Integer
+        Dim intLoopCount As Integer = 0
+        Dim intCurrentCollatz As Integer = intN
+
+        lbxOutput.Items.Clear()
+        lbxOutput.Items.Add("Iteration" & vbTab & "N")
+        lbxOutput.Items.Add(intLoopCount & vbTab & intCurrentCollatz)
+
+        While intCurrentCollatz <> 1 And intLoopCount <= intLoopLimit
+            intCurrentCollatz = iterateCollatz(intCurrentCollatz)
+            intLoopCount = intLoopCount + 1
+            lbxOutput.Items.Add(intLoopCount & vbTab & intCurrentCollatz)
+        End While
+
+        If intLoopCount > intLoopLimit Then Return -1
+
+        MsgBox(intN & " Reaches 1 By the Collatz Conjecture After " & intLoopCount & " Iterations")
+
+        Return intLoopCount
+    End Function
+
+    Private Sub btnLoopCollatz_Click(sender As Object, e As EventArgs) Handles btnLoopCollatz.Click
+        Dim intInput As Integer
+        Try
+            intInput = CInt(txtInput.Text)
+        Catch ex As Exception
+            MsgBox("Your input to the Collatz Conjecture must be an Integer")
+            Return
+        End Try
+
+        loopCollatz(intInput, lbxSingleCollatz)
+
+    End Sub
 End Class
