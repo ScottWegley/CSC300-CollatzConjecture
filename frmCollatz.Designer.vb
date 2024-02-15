0156c0d8bbb44d626517d41c9a746b143c797316
Scott Wegley <scbrwegley@protonmail.ch>
2024-02-15 15:16:51 -0800

    limit form size

--- frmCollatz.Designer.vb
+++ frmCollatz.Designer.vb
@@ -110,7 +110,7 @@ Partial Class frmCollatz
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(304, 450)
         Me.Controls.Add(Me.btnFirst20)
         Me.Controls.Add(Me.btnNextN)
         Me.Controls.Add(Me.txtNextIteration)
